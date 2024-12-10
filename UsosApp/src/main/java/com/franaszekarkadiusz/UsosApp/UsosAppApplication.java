aa7ffcef3e351aa6cb638b474249e48d2499c850
Arkadiusz Franaszek <franaszekarkadiusz@gmail.com>
2024-12-10 16:17:29 +0100

    first commit

---
+++ UsosApp/src/main/java/com/franaszekarkadiusz/UsosApp/UsosAppApplication.java
@@ -0,0 +1,13 @@
+package com.franaszekarkadiusz.UsosApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsosAppApplication.class, args);
+	}
+
+}
---
+++ UsosApp/src/test/java/com/franaszekarkadiusz/UsosApp/UsosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franaszekarkadiusz.UsosApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
